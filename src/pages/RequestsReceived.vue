01dbca5a76a653ee5ddc7d743e2470e44e2c9958
teredet <artem.vanzytler@gmail.com>
2023-03-30 07:48:15 +0300

    Added Requests

--- src/pages/RequestsReceived.vue
+++ src/pages/RequestsReceived.vue
@@ -1,13 +1,45 @@
 <template>
-    <h3>Requests</h3>
+    <section>
+        <base-card>
+            <header>
+                <h3>Request Received</h3>
+            </header>
+            <ul v-if="hasRequests">
+                <request-item v-for="req in requests" :key="req.id" :email="req.email"
+                    :message="req.message"></request-item>
+            </ul>
+            <h3 v-else>You haven't received any request yet!</h3>
+        </base-card>
+    </section>
 </template>
 
 
 <script>
+import { mapGetters } from 'vuex';
+import RequestItem from '../components/RequestItem.vue';
 
+export default {
+    components: { RequestItem },
+    computed: {
+        ...mapGetters(['requests', 'hasRequests']),
+    }
+}
 </script>
 
 
 <style scoped>
+header {
+    text-align: center;
+}
 
+ul {
+    list-style: none;
+    margin: 2rem auto;
+    padding: 0;
+    max-width: 30rem;
+}
+
+h3 {
+    text-align: center;
+}
 </style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,14 +1,23 @@
+import * as _ from 'lodash';
+
 export default {
     coaches(state) {
         return state.coaches;
     },
     hasCoaches(state) {
-        return !!state.coaches.length;
+        return !_.isEmpty(state.coaches);
     },
     userId(state) {
         return state.userId;
     },
     isCoach(state) {
         return state.coaches.some(coach => coach.id === state.userId);
+    },
+    requests(state, getters) {
+        const curentUserId = getters.userId;
+        return state.requests.filter(req => req.coachId === curentUserId);
+    },
+    hasRequests(_1, getters) {
+        return !_.isEmpty(getters.requests);
     }
 };
